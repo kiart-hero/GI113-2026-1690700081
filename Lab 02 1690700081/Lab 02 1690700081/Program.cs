de9273fd2537e5be83fa69192d069cd32a98ca1e
kiart-hero <kiart.losa@bumail.net>
2026-08-27 09:56:08 +0700

    Add lab02headder

---
+++ Lab 02 1690700081/Lab 02 1690700081/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_02_1690700081
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
